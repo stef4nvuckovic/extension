f268f144281f3174e7bc0f56c594ec20c1b2d6b5
Yuki <yuki.ouchi.jcfl@gmail.com>
2023-11-12 01:22:09 -0500

    weather updated
    Geolocation incorporated, UI improved.

--- weather.js
+++ weather.js
@@ -1,25 +1,39 @@
-//use of getCurrentPosition() from HTML5 Geolocation API
-
-
-//Check if Geolocation is supported on the browser
-if(navigator.geolocation){
-  //api利用可能の処理
-  navigator.geolocation.getCurrentPosition(successCallback, errorCallback)
-}
-
-//if supported, run getCurrentPosition() method and return the latitude and longitude 
-
-//if not, 
-else{
-  alert("Geolocation is not supported on this browser")
-}
+//_______________________ geolocation api____________________________________________________________________________
+function callApi(){
+  if(navigator.geolocation){
+    //api利用可能の処理
+    console.log("Geolocation available");
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback,{});//use of getCurrentPosition() from HTML5 Geolocation API
+  }
+  else{
+    console.log("Geolocation is not supported on this browser");
+  }
+};
 
 //out put the coordinates object 
 
-function successCallback(){
-
+function successCallback(pos){
+  var latitude = pos.coords.latitude
+  var longitude = pos.coords.longitude
+  console.log("Latitude: " + latitude, "Longitude: " + longitude);
 }
 
-function errorCallback(){
-
+function errorCallback(err){
+  console.log(err.message);
 }
+//___________________________________________________________________________________________________________________________
+
+//_______________________ weather api____________________________________________________________________________
+//api key  a74f7a8919839ec9c2f144b5cfcddca0
+
+
+//http://api.openweathermap.org/data/2.5/weather?lat=44.6963303&lon=-73.4623454&APPID=216a92274ae99ff3563ede25851a3c6f
+//Latitude:44.6963303  Longitude: -73.4623454
+
+
+//___________________________________________________________________________________________________________________________
+
+
+
+
+callApi();
